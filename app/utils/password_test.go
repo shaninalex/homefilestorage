f4decd6194fed993a3c1559130f8d075568a2fcd
shanart <shanin.x@gmail.com>
2023-01-25 09:01:32 +0200

    tests

--- app/utils/password_test.go
+++ app/utils/password_test.go
@@ -1,7 +1,24 @@
 package utils
 
-import "testing"
+import (
+	"log"
+	"testing"
+)
 
-func testComparingPassword(t *testing.T) {
+var test_password string = "test_password"
 
+func TestComparingPassword(t *testing.T) {
+	hashed_password, err := GenerateFromPassword(test_password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := CheckPassword(test_password, hashed_password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if res != true {
+		t.Errorf("Wrong password: %s and testing password: %s", hashed_password, test_password)
+	}
 }
