2770240fd3c3c7583e614bc632e4c155a445719d
shanart <shanin.x@gmail.com>
2023-01-27 11:15:57 +0200

    update scheme

--- app/database/scheme/scheme.sql
+++ app/database/scheme/scheme.sql
@@ -1,17 +1,17 @@
 -- SYSTEM
-CREATE TABLE "users"
+CREATE TABLE IF NOT EXISTS "users"
 (
     "id" SERIAL PRIMARY KEY,
     "email" CHAR(256) NOT NULL UNIQUE,
     "hashed_password" TEXT NOT NULL,
     "active" BOOLEAN NOT NULL DEFAULT true,
-    "created_at" TIMESTAMP DEFAULT Now()
+    "created_at" DATETIME DEFAULT current_timestamp
 );
 
-CREATE INDEX "users_email_idx" ON "users" ( "email" );
+CREATE INDEX IF NOT EXISTS "users_email_idx" ON "users" ( "email" );
 
 
-CREATE TABLE "files"
+CREATE TABLE IF NOT EXISTS "files"
 (
     "id" SERIAL PRIMARY KEY,
     "name" CHAR(256) NOT NULL UNIQUE,
@@ -20,38 +20,38 @@ CREATE TABLE "files"
     "system_path" CHAR(256),
     "owner" INTEGER NOT NULL,
     "public" BOOLEAN NOT NULL DEFAULT true,
-    "created_at" TIMESTAMP DEFAULT Now()
+    "created_at" DATETIME DEFAULT current_timestamp
     CHECK ("size" > 0),
     FOREIGN KEY ("owner") REFERENCES "users" ("id") ON DELETE CASCADE
 );
 
-CREATE INDEX "files_name_idx" ON "files" ( "name" );
+CREATE INDEX IF NOT EXISTS "files_name_idx" ON "files" ( "name" );
 
 
-CREATE TABLE "folders"
+CREATE TABLE IF NOT EXISTS "folders"
 (
     "id" SERIAL PRIMARY KEY,
     "name" CHAR(256) NOT NULL,
     "color" CHAR(256) NOT NULL,
     "owner" INTEGER NOT NULL,
-    "created_at" TIMESTAMP DEFAULT Now(),
+    "created_at" DATETIME DEFAULT current_timestamp,
     FOREIGN KEY ("owner") REFERENCES "users" ("id") ON DELETE CASCADE
 );
 
-CREATE INDEX "folders_name_idx" ON "folders" ( "name" );
+CREATE INDEX IF NOT EXISTS "folders_name_idx" ON "folders" ( "name" );
 
 -- BOOKSHELF
-CREATE TABLE "categories"
+CREATE TABLE IF NOT EXISTS "categories"
 (
     "category_name" CHAR(32) NOT NULL PRIMARY KEY,
     "user_id" INTEGER NOT NULL,
-    "created_at" TIMESTAMP DEFAULT Now(),
+    "created_at" DATETIME DEFAULT current_timestamp,
     FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE
 );
 
-CREATE INDEX "categories_category_name_idx" ON "categories" ( "category_name" );
+CREATE INDEX IF NOT EXISTS "categories_category_name_idx" ON "categories" ( "category_name" );
 
-CREATE TABLE "books"
+CREATE TABLE IF NOT EXISTS "books"
 (
     "id" SERIAL PRIMARY KEY,
     "name" CHAR(256) NOT NULL,
@@ -62,10 +62,10 @@ CREATE TABLE "books"
     "isbn" TEXT,
     FOREIGN KEY ("file_id") REFERENCES "files" ("id") ON DELETE SET NULL
 );
-CREATE INDEX "books_name_idx" ON "books" ( "name" );
+CREATE INDEX IF NOT EXISTS "books_name_idx" ON "books" ( "name" );
 
 
-CREATE TABLE "books_categories"
+CREATE TABLE IF NOT EXISTS "books_categories"
 (
     "book_id" INTEGER NOT NULL,
     "category" CHAR(32) NOT NULL,
@@ -74,19 +74,19 @@ CREATE TABLE "books_categories"
 );
 
 
-CREATE TABLE "authors"
+CREATE TABLE IF NOT EXISTS "authors"
 (
     "id" SERIAL PRIMARY KEY,
     "fullname" CHAR(256) NOT NULL,
     "birthday" INTEGER,
     "death" INTEGER,
     "user_id" INTEGER NOT NULL,
-    "created_at" TIMESTAMP DEFAULT Now(),
+    "created_at" DATETIME DEFAULT current_timestamp,
     FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE
 );
-CREATE INDEX "authors_name_idx" ON "authors" ("fullname");
+CREATE INDEX IF NOT EXISTS "authors_name_idx" ON "authors" ("fullname");
 
-CREATE TABLE "author_books"
+CREATE TABLE IF NOT EXISTS "author_books"
 (
     "author_id" INTEGER NOT NULL,
     "book_id" INTEGER NOT NULL,
@@ -94,23 +94,23 @@ CREATE TABLE "author_books"
     FOREIGN KEY ("book_id") REFERENCES "books" ("id") ON DELETE SET NULL
 );
 
--- SYSTEM ACCESS RELATED
-CREATE TABLE "access_log"
+-- -- SYSTEM ACCESS RELATED
+CREATE TABLE IF NOT EXISTS "access_log"
 (
     "file_id" INTEGER NOT NULL,
     "user_id" INTEGER NOT NULL,
-    "access_time" TIMESTAMP DEFAULT Now(),
+    "access_time" DATETIME DEFAULT current_timestamp,
     FOREIGN KEY ("file_id") REFERENCES "files" ("id") ON DELETE CASCADE,
     FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE
 );
 
 
--- TOOD: videos
--- This app will be able to store not only books, but also videos. Why?
--- Because on my laptop I have 4-5 downloaded courses and it will be convinuent
--- to store them to. May be I can create some streaming service to watch them from 
--- device.
+-- -- TOOD: videos
+-- -- This app will be able to store not only books, but also videos. Why?
+-- -- Because on my laptop I have 4-5 downloaded courses and it will be convinuent
+-- -- to store them to. May be I can create some streaming service to watch them from 
+-- -- device.
 
--- TODO:
---      - mock data
---      - develop and create functions
+-- -- TODO:
+-- --      - mock data
+-- --      - develop and create functions
