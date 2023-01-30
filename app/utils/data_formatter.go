f065fcdd74a34e7d8b570f723e3e1b4abdc7f669
shaninalex <shanin.x@gmail.com>
2023-01-30 10:09:53 +0200

    public user object

--- app/utils/data_formatter.go
+++ app/utils/data_formatter.go
@@ -1,10 +1,25 @@
 package utils
 
-import "homestorage/app/database"
+import (
+	"homestorage/app/database"
+	"time"
+)
 
-func PublicUser(user *database.User) *database.User {
+type PublicUserObject struct {
+	Id         int        `json:"id"`
+	Email      string     `json:"email"`
+	Active     bool       `json:"active"`
+	Created_at *time.Time `json:"created_at"`
+}
+
+func PublicUser(user *database.User) *PublicUserObject {
 	// We need to remove hashed password from public available
 	// user object
-	user.Hashed_password = ""
-	return user
+	public_user := &PublicUserObject{
+		Id:         user.Id,
+		Email:      user.Email,
+		Active:     user.Active,
+		Created_at: user.Created_at,
+	}
+	return public_user
 }
