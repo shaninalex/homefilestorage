addce8fda5fb07e575cab33507abcaca375e1fe6
alex-digisol <alex@digisol.agency>
2023-01-31 18:33:49 +0200

    remove handmade type chacker

--- app/filesystem/utils.go
+++ app/filesystem/utils.go
@@ -10,36 +10,6 @@ import (
 
 var (
 	MAX_FILE_SIZE = 314572800 // 300Mb ( NOTE: This constant can be in configuration files )
-
-	ALLOWED_FILE_TYPES = [19]string{
-
-		// DOCUMENTS and PRESENTATIONS
-		"application/msword", // .doc
-		"application/vnd.openxmlformats-officedocument.wordprocessingml.document",   // .docx
-		"application/vnd.ms-powerpoint",                                             // .ppt
-		"application/vnd.openxmlformats-officedocument.presentationml.presentation", // .pptx
-		"application/vnd.oasis.opendocument.presentation",                           // .odp
-		"application/vnd.oasis.opendocument.spreadsheet",                            // .ods
-		"application/vnd.oasis.opendocument.text",                                   // .odt
-		"application/rtf",
-		"application/pdf", // .pdf
-		"text/plain",
-
-		// IMAGES
-		"image/jpeg", //.jpeg .jpg
-		"image/png",  // png
-
-		// AUDIO
-		"audio/mpeg", // mp3
-		"audio/3gpp", // 3gp
-		"audio/ogg",  // oga
-
-		// VIDEO
-		"video/mpeg",      // .mpeg
-		"video/x-msvideo", // .avi
-		"video/ogg",       // .ogv
-		"video/mp4",       // .mp4
-	}
 )
 
 var (
@@ -55,15 +25,6 @@ func isValidMaxSize(file_size int64) bool {
 	return true
 }
 
-func isAllowedFIleType(str string) bool {
-	for _, v := range ALLOWED_FILE_TYPES {
-		if v == str {
-			return true
-		}
-	}
-	return false
-}
-
 func ValidateFile(file_size int64, mime_type string) error {
 
 	if isValidMaxSize(file_size) != true {
@@ -72,10 +33,6 @@ func ValidateFile(file_size int64, mime_type string) error {
 
 	if file_size == 0 {
 		return errors.New(ERROR_EMPTY_FILE)
-	}
-
-	if isAllowedFIleType(mime_type) != true {
-		return errors.New(ERROR_NOT_ALLOWED_FILE_TYPE)
 	}
 
 	return nil
