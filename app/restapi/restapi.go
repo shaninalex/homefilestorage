173337c33827034026a6f4dc5cd250cbf1833181
shaninalex <shanin.x@gmail.com>
2023-02-02 18:08:59 +0200

    get files

--- app/restapi/restapi.go
+++ app/restapi/restapi.go
@@ -42,13 +42,16 @@ func Server(db *database.DatabaseRepository, storage_path string, port int) {
 	h := Handlers(db, storage_path)
 
 	router.GET("/", h.RouteIndex)
-	router.GET("/api/v1/account/", h.RouteGetAccount) // require token
-	router.POST("/api/v1/account/create/", h.RouteCreateUser)
-	router.POST("/api/v1/account/login/", h.RouteLoginUser)
-	router.POST("/api/v1/account/refresh/", h.RouteRefreshToken) // require token
+	router.GET("/api/v1/account/", h.RouteGetAccount)
+	router.POST("/api/v1/account/create/", h.RouteCreateUser) // public
+	router.POST("/api/v1/account/login/", h.RouteLoginUser)   // public
+	router.POST("/api/v1/account/refresh/", h.RouteRefreshToken)
 
-	router.POST("/api/v1/files/upload/", h.RouteSaveFile) // require token
+	router.POST("/api/v1/files/upload/", h.RouteSaveFile)
 	router.GET("/api/v1/files/", h.RouteFilesList)
+	router.GET("/api/v1/file/:id/", h.RouteFileItem)
+
+	// public ( shared links )
 
 	log.Printf("Start server under :%d port...", port)
 	http.ListenAndServe(fmt.Sprintf(":%d", port), router)
