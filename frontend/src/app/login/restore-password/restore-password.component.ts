3611120e07b5772a3c068e181acf03b5aac3499a
shaninalex <shanin.x@gmail.com>
2023-04-08 08:34:03 +0300

    back to work again

--- frontend/src/app/login/restore-password/restore-password.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-restore-password',
-  templateUrl: './restore-password.component.html'
-})
-export class RestorePasswordComponent {
-
-}
