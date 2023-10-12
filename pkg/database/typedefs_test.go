8260454e70ec28439b100dbc6bb6d2d2caf412f2
shaninalex <shanin.x@gmail.com>
2023-10-12 09:04:41 +0300

    #53 account password hashing

--- pkg/database/typedefs_test.go
+++ pkg/database/typedefs_test.go
@@ -2,21 +2,7 @@ package database
 
 import "testing"
 
-func TestAccountCheckPassword(t *testing.T) {
-
-	account := &Account{
-		ID:           1,
-		Email:        "test@test.com",
-		Name:         "test",
-		PasswordHash: "password", // TODO: actual hash password
-	}
-
-	if !account.CheckPassword("password") {
-		t.Error("Account.CheckPassword does not success")
-	}
-}
-
-func TestAccountHashPassword(t *testing.T) {
+func TestAccountPassword(t *testing.T) {
 
 	account := &Account{
 		ID:    1,
